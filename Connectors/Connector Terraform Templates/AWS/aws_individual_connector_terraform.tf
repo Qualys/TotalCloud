55b68247d8de73972c28a24a6467a71aa7257296
Yash Jhunjhunwala <109724146+yjhunjhunwala1@users.noreply.github.com>
2024-06-25 12:04:12 +0530

    Update aws_individual_connector_terraform.tf

--- Connectors/Connector Terraform Templates/AWS/aws_individual_connector_terraform.tf
+++ Connectors/Connector Terraform Templates/AWS/aws_individual_connector_terraform.tf
@@ -7,45 +7,42 @@
 # INPUT THE FOLLOWING PARAMETERS
 #
 # username: Qualys Platform Username
-# Password: Qualys Platform Password
+# password: Qualys Platform Password
 # baseurl: Qualys Platform API server for URL
 ##################################
 
 variable "connector_type" {
-  type    = string
+  type        = string
   description = "If set to CSA creates CSMP Connector ; If set to AI creates AssetView Connector ; Default is CSA"
-  default = "CSA"
+  default     = "CSA"
+}
 
-}
 variable "username" {
-  type    = string
+  type        = string
   description = "Qualys Platform Username."
+  sensitive   = true
 }
-variable "baseurl" {
-  type    = string
-  description = "Qualys Platform API server, You can find Platform Wise URL here :- https://www.qualys.com/platform-identification/"
-  default = "https://qualysapi.qg2.apps.qualys.com"
-}
+
 variable "password" {
-  type    = string
+  type        = string
   description = "Qualys Platform Password."
+  sensitive   = true
 }
+
 variable "externalId" {
-  type    = string
+  type        = string
   description = "ExternalId for the assume role."
 }
 
-#############################
-# Initializing the provider
-##############################
-
-provider "aws" {
-  region  = "us-east-1"
+variable "baseurl" {
+  type        = string
+  description = "Qualys Platform API server, You can find Platform Wise URL here :- https://www.qualys.com/platform-identification/"
+  default     = "https://qualysapi.qg1.apps.qualys.ca"
 }
 
-################################################
-# Creating an IAM role with assume_role policy
-################################################
+provider "aws" {
+  region = "us-east-1"
+}
 
 resource "aws_iam_role" "QualysReadOnlyRole" {
   name = "QualysCSPMReadOnlyRole"
@@ -61,104 +58,113 @@ resource "aws_iam_role" "QualysReadOnlyRole" {
         },
         "Condition": {
           "StringEquals": {
-            "sts:ExternalId": "${var.externalId}"
+            "sts:ExternalId": var.externalId
           }
-      }
-    }
-  ]
-})
-}
-
-resource "aws_iam_policy" "QualysCSPMReadOnlyPolicy" {
-  name        = "QualysCSPMReadOnlyPolicy"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        "Sid" : "QualysCustomPolicyPermissions",
-        Action = [
-          "states:DescribeStateMachine", "elasticfilesystem:DescribeFileSystemPolicy", "qldb:ListLedgers", "qldb:DescribeLedger", "kafka:ListClusters", "codebuild:BatchGetProjects", "wafv2:GetWebACLForResource", "backup:ListBackupVaults", "backup:DescribeBackupVault", "ec2:GetEbsEncryptionByDefault", "ec2:GetEbsDefaultKmsKeyId", "guardduty:ListDetectors", "guardduty:GetDetector", "glue:GetDataCatalogEncryptionSettings", "elasticmapreduce:GetBlockPublicAccessConfiguration", "lambda:GetFunctionConcurrency", "ds:ListLogSubscriptions", "ssm:getdocument", "ssm:getservicesetting", "states:GetExecutionHistory", "eks:ListFargateProfiles", "eks:DescribeFargateProfile"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-      {
-        "Sid" : "QualysAPIGatewayGetPermissions",
-        "Effect" : "Allow",
-        "Action" : "apigateway:GET",
-        "Resource" : "arn:aws:apigateway:*::/restapis/*"
+        }
       }
     ]
   })
 }
 
-###################################################
-# Attaching SecurityAudit and Custom Read Only Policy to the Qualys role
-###################################################
+resource "aws_iam_policy" "QualysCSPMReadOnlyPolicy" {
+  name   = "QualysCSPMReadOnlyPolicy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        "Sid": "QualysCustomPolicyPermissions",
+        "Effect": "Allow",
+        "Action": [
+          "states:DescribeStateMachine", "elasticfilesystem:DescribeFileSystemPolicy", "qldb:ListLedgers", "qldb:DescribeLedger", "kafka:ListClusters", "codebuild:BatchGetProjects", "wafv2:GetWebACLForResource", "backup:ListBackupVaults", "backup:DescribeBackupVault", "ec2:GetEbsEncryptionByDefault", "ec2:GetEbsDefaultKmsKeyId", "guardduty:ListDetectors", "guardduty:GetDetector", "glue:GetDataCatalogEncryptionSettings", "elasticmapreduce:GetBlockPublicAccessConfiguration", "lambda:GetFunctionConcurrency", "ds:ListLogSubscriptions", "ssm:getdocument", "ssm:getservicesetting", "states:GetExecutionHistory", "eks:ListFargateProfiles", "eks:DescribeFargateProfile"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Sid": "QualysAPIGatewayGetPermissions",
+        "Effect": "Allow",
+        "Action": "apigateway:GET",
+        "Resource": "arn:aws:apigateway:*::/restapis/*"
+      }
+    ]
+  })
+}
 
 resource "aws_iam_role_policy_attachment" "system_defined_policy_attach" {
-  role       = "${aws_iam_role.QualysReadOnlyRole.name}"
+  role       = aws_iam_role.QualysReadOnlyRole.name
   policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
-
 }
+
 resource "aws_iam_role_policy_attachment" "custom_policy_attach" {
-  role       = "${aws_iam_role.QualysReadOnlyRole.name}"
-  policy_arn = "${aws_iam_policy.QualysCSPMReadOnlyPolicy.arn}"
+  role       = aws_iam_role.QualysReadOnlyRole.name
+  policy_arn = aws_iam_policy.QualysCSPMReadOnlyPolicy.arn
 }
-
-#################################################################
-# Qualys API Call to create CloudView AWS Connector
-#################################################################
 
 resource "local_file" "authentication_key" {
-  content  = jsonencode ({
-  "ServiceRequest": {
-    "data": {
-      "AwsAssetDataConnector": {
-        "name": "AWS-connector-${substr("${aws_iam_role.QualysReadOnlyRole.arn}",13,12)}",
-        "arn": "${aws_iam_role.QualysReadOnlyRole.arn}",
-        "externalId": "${var.externalId}",
-        "allRegions": "true",
-        "runFrequency": "240",
-        "activation": {
-          "set": {
-            "ActivationModule": [
-              "VM",
-              "PC"
-            ]
-          }
-        },
-        "connectorAppInfos": {
-          "set": {
-            "ConnectorAppInfoQList": [
-              {
-                "set": {
-                  "ConnectorAppInfo": {
-                    "name": "${var.connector_type}",
-                    "identifier": "${aws_iam_role.QualysReadOnlyRole.arn}"
+  content = jsonencode({
+    "ServiceRequest": {
+      "data": {
+        "AwsAssetDataConnector": {
+          "name": "AWS-connector-${substr(aws_iam_role.QualysReadOnlyRole.arn, 13, 12)}",
+          "arn": aws_iam_role.QualysReadOnlyRole.arn,
+          "externalId": var.externalId,
+          "allRegions": "true",
+          "runFrequency": "240",
+          "activation": {
+            "set": {
+              "ActivationModule": [
+                "VM",
+                "PC"
+              ]
+            }
+          },
+          "connectorAppInfos": {
+            "set": {
+              "ConnectorAppInfoQList": [
+                {
+                  "set": {
+                    "ConnectorAppInfo": {
+                      "name": var.connector_type,
+                      "identifier": aws_iam_role.QualysReadOnlyRole.arn
+                    }
                   }
                 }
-              }
-            ]
+              ]
+            }
           }
         }
       }
     }
-  }
-})
-  filename = "${path.module}/file.json"
-  depends_on = [aws_iam_role_policy_attachment.system_defined_policy_attach,aws_iam_role_policy_attachment.custom_policy_attach]
+  })
+  filename   = "${path.module}/file.json"
+  depends_on = [
+    aws_iam_role_policy_attachment.system_defined_policy_attach,
+    aws_iam_role_policy_attachment.custom_policy_attach
+  ]
 }
+
 module "QualysCloudViewAssetViewConnector" {
   source  = "matti/resource/shell"
-  command = "curl -u '${var.username}:${var.password}' --header 'Content-Type: application/json' --header 'Accept: application/xml' -X POST --data-binary @- ${var.baseurl}/qps/rest/3.0/create/am/awsassetdataconnector < file.json"
-  depends = [aws_iam_role_policy_attachment.system_defined_policy_attach,aws_iam_role_policy_attachment.custom_policy_attach]
+  command = <<EOT
+    curl -u '${var.username}:${var.password}' \
+    --header 'Content-Type: application/json' \
+    --header 'Accept: application/xml' \
+    -X POST \
+    --data-binary @- ${var.baseurl}/qps/rest/3.0/create/am/awsassetdataconnector < file.json
+  EOT
+  depends_on = [
+    aws_iam_role_policy_attachment.system_defined_policy_attach,
+    aws_iam_role_policy_attachment.custom_policy_attach
+  ]
 }
 
-#######################################################
-# Outputs
-#######################################################
+output "ROLE_ARN" {
+  value = aws_iam_role.QualysReadOnlyRole.arn
+}
 
-output "ROLE_ARN" { value = aws_iam_role.QualysReadOnlyRole.arn}
-output "CLOUDVIEW-OUTPUT" { value = module.QualysCloudViewAssetViewConnector.stdout }
-output "CLOUDVIEW-EXIT-STATUS" { value = module.QualysCloudViewAssetViewConnector.exitstatus }
+output "CLOUDVIEW-OUTPUT" {
+  value = module.QualysCloudViewAssetViewConnector.stdout
+}
+
+output "CLOUDVIEW-EXIT-STATUS" {
+  value = module.QualysCloudViewAssetViewConnector.exitstatus
+}
