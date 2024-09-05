485a71e112b8e480da1008d8ca2edc63abe6e9eb
weir <weir@bluehexagon.ai>
2024-09-05 17:41:56 -0500

    CDR-1324 Add option to create qualys service account or not

--- GCP/ProjectLevelConnector/Terraform/terraform.tfvars
+++ GCP/ProjectLevelConnector/Terraform/terraform.tfvars
@@ -1,1 +1,2 @@
 project_id = "<project_id>"
+CreateQualysCSPMServiceAccount = true
--- GCP/ProjectLevelConnector/Terraform/variables.tf
+++ GCP/ProjectLevelConnector/Terraform/variables.tf
@@ -3,8 +3,14 @@ variable "project_id" {
   type        = string
 }
 
+variable "CreateQualysCSPMServiceAccount" {
+  description = "Set to true if create Qualys CSPM Service Account, else set to false"
+  type        = bool
+  default     = true
+}
 variable "service_account_id" {
   description = "Service account ID"
   type        = string
   default     = "qualyscspmserviceaccount"
 }
+
