9ab9d5419c1e0c789578c9e2a5b621da0873186c
weir <weir@bluehexagon.ai>
2024-09-06 12:54:57 -0500

    Fix provider error in 4.x

--- Azure/SubscriptionConnector/Terraform/provider.tf
+++ Azure/SubscriptionConnector/Terraform/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
+      version = ">= 4.0.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
@@ -15,6 +15,7 @@ terraform {
   }
 }
 provider "azurerm" {
+  subscription_id = var.subscription_id
   features {}
 }
 provider "azuread" {}
